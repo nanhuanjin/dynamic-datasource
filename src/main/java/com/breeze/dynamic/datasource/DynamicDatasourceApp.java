329172551abd5fd084415496040e9d5bb698305d
breeze <breeze67@163.com>
2020-06-05 17:18:40 +0800

    feat：项目初始化

---
+++ src/main/java/com/breeze/dynamic/datasource/DynamicDatasourceApp.java
@@ -0,0 +1,15 @@
+package com.breeze.dynamic.datasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author breeze
+ * @date 2020/6/5
+ */
+@SpringBootApplication
+public class DynamicDatasourceApp {
+    public static void main(String[] args) {
+        SpringApplication.run(DynamicDatasourceApp.class, args);
+    }
+}
